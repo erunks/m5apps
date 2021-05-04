f77d209dd759a197feb569ecb4bd3d8223370fc4
Edward Runkel <eddierunkel@gmail.com>
2021-05-04 13:58:28 -0400

    Update connect script to kill the session when it is exited

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -4,4 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DEV_DEVICE=$( $DIR/get_device_location.sh )
 
 # open connection to the M5stack
-`screen $DEV_DEVICE 115200`
+`screen -S M5Stack $DEV_DEVICE 115200`
+
+# kill screen session
+`screen -X -S M5Stack quit`
